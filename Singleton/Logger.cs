31446c10016d21d86fcce4d6ddeffbdc744e3b6a
Amir Doosti <amirdoosti@hotmail.com>
2024-04-27 10:42:46 +0400

    Added Singleton Implementation

---
+++ Singleton/Logger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Logger
+    {
+        private static Logger? instance;
+        private static readonly object lockObject = new object();
+
+        private Logger() { }
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Logger();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void Log(string message)
+        {
+            // Log the message
+            Console.WriteLine($"[INFO] {message}");
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,10 @@
+﻿using Singleton;
+
+Console.WriteLine("Singleton Design Pattern (Creational Design Pattern)");
+
+// Get the singleton instance of the logger
+Logger logger = Logger.Instance;
+
+// Log messages
+logger.Log("Logging message 1");
+logger.Log("Logging message 2");
