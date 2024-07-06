7c0a861e4cdd0257ff062dfe50e0b21ad1b4854f
Amir Doosti <amirdoosti@hotmail.com>
2024-07-06 12:00:26 +0400

    Added Proxy Design Pattern

---
+++ Proxy/Program.cs
@@ -0,0 +1,11 @@
+﻿using Proxy;
+
+Console.WriteLine("Flyweight Design Pattern (Structural Design Pattern)\n");
+
+IImage image = new ProxyImage("test_image.jpg");
+
+// Image will be loaded from disk
+image.Display();
+
+// Image will not be loaded from disk
+image.Display();
---
+++ Proxy/RealImage.cs
@@ -0,0 +1,23 @@
+﻿namespace Proxy
+{
+    public class RealImage : IImage
+    {
+        private string _fileName;
+
+        public RealImage(string fileName)
+        {
+            _fileName = fileName;
+            LoadFromDisk();
+        }
+
+        private void LoadFromDisk()
+        {
+            Console.WriteLine("Loading " + _fileName);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Displaying " + _fileName);
+        }
+    }
+}
